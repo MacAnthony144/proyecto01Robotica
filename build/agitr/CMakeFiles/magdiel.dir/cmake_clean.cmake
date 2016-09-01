f9ea014a483ea5ee162276a3216a57be51fd363f
magdiel <jmagdielg@gmail.com>
2016-09-01 14:53:45 -0700

    inicio

---
+++ build/agitr/CMakeFiles/magdiel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/magdiel.dir/magdiel.cpp.o"
+  "/home/ros/catkin_ws/devel/lib/agitr/magdiel.pdb"
+  "/home/ros/catkin_ws/devel/lib/agitr/magdiel"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/magdiel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ros/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ros/Downloads/MacAnthony_144-robotica_proyecto1-3053938799e7/devel;/home/ros/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/ros/Downloads/MacAnthony_144-robotica_proyecto1-3053938799e7/devel;/home/ros/catkin_ws/devel;/opt/ros/indigo" != "" else []
